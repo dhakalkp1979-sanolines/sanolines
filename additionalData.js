b17c2f8b07d3c95e6bfb32e2c435ba9908d9ec37
dhakalkp1979-sanolines <dhakalkp1979@gmail.com>
2026-09-01 18:49:19 +0100

    Create additionalData.js

---
+++ additionalData.js
@@ -0,0 +1,62 @@
+export const additionalSections = [
+  {
+    title: "Healthcare",
+    description:
+      "Information about healthcare services, health centres, SNS and useful health resources in Portugal.",
+    link: "https://www.sns.gov.pt/"
+  },
+  {
+    title: "Education",
+    description:
+      "Information about schools, higher education, training and education services in Portugal.",
+    link: "https://eportugal.gov.pt/"
+  },
+  {
+    title: "Housing",
+    description:
+      "General information about renting, accommodation and housing services in Portugal.",
+    link: "https://eportugal.gov.pt/"
+  },
+  {
+    title: "Work",
+    description:
+      "Information about employment, contracts, workers' rights and finding work.",
+    link: "https://www.iefp.pt/"
+  },
+  {
+    title: "Family",
+    description:
+      "Information about family services, registration and family-related procedures.",
+    link: "https://eportugal.gov.pt/"
+  },
+  {
+    title: "Transportation",
+    description:
+      "Information about driving licences, public transport and transport services.",
+    link: "https://www.imt-ip.pt/"
+  },
+  {
+    title: "Taxes",
+    description:
+      "General information about Portuguese taxes and tax services.",
+    link: "https://www.portaldasfinancas.gov.pt/"
+  },
+  {
+    title: "Emergency Contacts",
+    description:
+      "Important emergency and public safety contact information in Portugal.",
+    link: "https://www.portugal.gov.pt/"
+  },
+  {
+    title: "Useful Forms",
+    description:
+      "Find official government forms and documents through Portuguese public services.",
+    link: "https://eportugal.gov.pt/"
+  },
+  {
+    title: "Portuguese Language Help",
+    description:
+      "Useful Portuguese words, phrases and language resources for everyday life.",
+    link: "https://pptonline.acm.gov.pt/"
+  }
+];
