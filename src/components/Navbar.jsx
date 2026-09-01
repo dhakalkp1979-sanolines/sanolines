662f0362335a17ebab4d2b64b521977f83b5e8d6
dhakalkp1979-sanolines <dhakalkp1979@gmail.com>
2026-09-01 18:19:28 +0100

    Create Navbar.jsx

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,18 @@
+function Navbar() {
+  return (
+    <header>
+      <nav>
+        <a href="#home">Sanolines</a>
+
+        <div>
+          <a href="#portugal">Portugal Services</a>
+          <a href="#visa">Visa & Immigration</a>
+          <a href="#calculators">Calculators</a>
+          <a href="#directory">Official Websites</a>
+        </div>
+      </nav>
+    </header>
+  );
+}
+
+export default Navbar;
