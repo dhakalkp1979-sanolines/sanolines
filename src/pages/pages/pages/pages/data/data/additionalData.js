342e959d1ceafd4b3968325d36a22060d8b220c5
dhakalkp1979-sanolines <dhakalkp1979@gmail.com>
2026-09-01 19:49:09 +0100

    Create additionalData.js

---
+++ src/pages/pages/pages/pages/data/data/additionalData.js
@@ -0,0 +1,52 @@
+export const additionalSections = [
+  {
+    title: "Healthcare",
+    description: "Information about healthcare services in Portugal.",
+    link: "https://www.sns.gov.pt/"
+  },
+  {
+    title: "Education",
+    description: "Information about education and training in Portugal.",
+    link: "https://eportugal.gov.pt/"
+  },
+  {
+    title: "Housing",
+    description: "Information about renting and housing services.",
+    link: "https://eportugal.gov.pt/"
+  },
+  {
+    title: "Work",
+    description: "Information about employment and workers' services.",
+    link: "https://www.iefp.pt/"
+  },
+  {
+    title: "Family",
+    description: "Information about family-related public services.",
+    link: "https://eportugal.gov.pt/"
+  },
+  {
+    title: "Transportation",
+    description: "Driving licence and transport information.",
+    link: "https://www.imt-ip.pt/"
+  },
+  {
+    title: "Taxes",
+    description: "Information about Portuguese tax services.",
+    link: "https://www.portaldasfinancas.gov.pt/"
+  },
+  {
+    title: "Emergency Contacts",
+    description: "Important emergency information in Portugal.",
+    link: "https://www2.gov.pt/en/servicos/ligar-para-o-numero-de-emergencia-112"
+  },
+  {
+    title: "Useful Forms",
+    description: "Find official Portuguese government forms and services.",
+    link: "https://eportugal.gov.pt/"
+  },
+  {
+    title: "Portuguese Language Help",
+    description: "Useful Portuguese language resources.",
+    link: "https://pptonline.acm.gov.pt/"
+  }
+];
