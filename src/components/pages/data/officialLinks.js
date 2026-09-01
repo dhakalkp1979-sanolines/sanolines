18128b5c9500a5dfb02b446ce134132350518c7d
dhakalkp1979-sanolines <dhakalkp1979@gmail.com>
2026-09-01 18:30:33 +0100

    Create officialLinks.js

---
+++ src/components/pages/data/officialLinks.js
@@ -0,0 +1,62 @@
+export const officialLinks = [
+  {
+    name: "AIMA",
+    description: "Immigration and residence services",
+    url: "https://aima.gov.pt/"
+  },
+  {
+    name: "NIF / Finanças",
+    description: "Tax identification and tax services",
+    url: "https://www.portaldasfinancas.gov.pt/"
+  },
+  {
+    name: "NISS / Segurança Social",
+    description: "Social Security identification and services",
+    url: "https://www.seg-social.pt/"
+  },
+  {
+    name: "SNS",
+    description: "National Health Service",
+    url: "https://www.sns.gov.pt/"
+  },
+  {
+    name: "IMT",
+    description: "Driving licence and transport services",
+    url: "https://www.imt-ip.pt/"
+  },
+  {
+    name: "IEFP",
+    description: "Employment and professional training",
+    url: "https://www.iefp.pt/"
+  },
+  {
+    name: "Justiça",
+    description: "Citizenship, civil registry and justice services",
+    url: "https://justica.gov.pt/"
+  },
+  {
+    name: "ePortugal",
+    description: "Portuguese public services portal",
+    url: "https://eportugal.gov.pt/"
+  },
+  {
+    name: "Chave Móvel Digital",
+    description: "Digital authentication service",
+    url: "https://www.autenticacao.gov.pt/"
+  },
+  {
+    name: "Portugal Visa Portal",
+    description: "Official information about Portuguese visas",
+    url: "https://vistos.mne.gov.pt/"
+  },
+  {
+    name: "Portuguese Consular Portal",
+    description: "Portuguese embassies and consular services",
+    url: "https://portaldascomunidades.mne.gov.pt/"
+  },
+  {
+    name: "VFS Global",
+    description: "Visa application services",
+    url: "https://www.vfsglobal.com/"
+  }
+];
