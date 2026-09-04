baf5edfd31c7014863ff1237833f7c6135ee4bda
dhakalkp1979-sanolines <dhakalkp1979@gmail.com>
2026-09-04 19:28:19 +0100

    Update countryServices.js

--- src/data/countryServices.js
+++ src/data/countryServices.js
@@ -2,12 +2,14 @@ import europeServices from "./europeServices";
 import asiaServices from "./asiaServices";
 import africaServices from "./africaServices";
 import northAmericaServices from "./northAmericaServices";
+import southAmericaServices from "./southAmericaServices";
 
 const countryServices = {
   ...europeServices,
   ...asiaServices,
   ...africaServices,
   ...northAmericaServices,
+  ...southAmericaServices,
 };
 
 export default countryServices;
