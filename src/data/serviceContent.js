425867bfef645117bf28d00bb4404a1b8aea6757
dhakalkp1979-sanolines <dhakalkp1979@gmail.com>
2026-09-04 23:05:52 +0100

    Create serviceContent.js

---
+++ src/data/serviceContent.js
@@ -0,0 +1,17 @@
+const serviceKeyMap = {
+  "Government & Official Services": "government",
+  "Immigration & Visa": "immigration",
+  "Jobs & Employment": "jobs",
+  "Money & Banking": "banking",
+  "Taxes": "taxes",
+  "Healthcare": "healthcare",
+  "Education": "education",
+  "Transport & Driving": "transport",
+  "Emergency Services": "emergency",
+  "Travel": "travel",
+  "Housing": "housing",
+  "Useful Contacts": "contacts",
+  "Calculators & Tools": "calculators",
+};
+
+export default serviceKeyMap;
