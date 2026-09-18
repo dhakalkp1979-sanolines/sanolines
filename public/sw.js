6ef8d225b4452a7ba6cb4db726077899d4ae4813
dhakalkp1979-sanolines <dhakalkp1979@gmail.com>
2026-09-18 19:15:15 +0100

    Create sw.js

---
+++ public/sw.js
@@ -0,0 +1,40 @@
+const CACHE_NAME = "sanolines-v1";
+
+const OFFLINE_URLS = [
+  "/",
+  "/index.html",
+  "/favicon.ico",
+  "/manifest.json"
+];
+
+self.addEventListener("install", (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(OFFLINE_URLS);
+    })
+  );
+
+  self.skipWaiting();
+});
+
+self.addEventListener("activate", (event) => {
+  event.waitUntil(
+    caches.keys().then((cacheNames) => {
+      return Promise.all(
+        cacheNames
+          .filter((name) => name !== CACHE_NAME)
+          .map((name) => caches.delete(name))
+      );
+    })
+  );
+
+  self.clients.claim();
+});
+
+self.addEventListener("fetch", (event) => {
+  event.respondWith(
+    fetch(event.request).catch(() => {
+      return caches.match(event.request);
+    })
+  );
+});
